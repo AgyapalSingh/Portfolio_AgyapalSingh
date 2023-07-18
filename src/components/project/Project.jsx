a2433cfd8c3760817bb792818a6015493c251dde
AGYAPAL SINGH <107750888+AgyapalSingh@users.noreply.github.com>
2023-07-18 12:20:07 +0530

    New Projects Added

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./project.css"
-import IMG1 from '../../assets/SongifyTemplate/Songify1.png'
-import IMG3 from '../../assets/EcommerceTemplate/Kart1.png'
-import IMG2 from '../../assets/MovieTemplate/MovieMaster.png'
-import IMG5 from '../../assets/DesiShopTemplate/DesiShop1.png'
+import IMG1 from '../../assets/MernStackProject/E-Shop.png'
+import IMG2 from '../../assets/MernStackProject/SocialWeb.png'
+import IMG3 from '../../assets/ReactProject/MovieMaster.png'
+import IMG4 from '../../assets/ReactProject/Ipod.png'
 
 const Project = () => {
     return (
@@ -14,23 +14,23 @@ const Project = () => {
             <div className="container project_container">
                 <article className='project_item'>
                     <div className="project_item-image">
-                        <img src={IMG5} alt="desishop" />
+                        <img src={IMG1} alt="e-shop" />
                     </div>
                     <h3>E-Shop - Ecommerce website</h3>
                     <h5 className='text-light'>[ Complete Ecommerce Website]</h5>
-                    <p>Desi Shop is an Ecommerce webapplication created using MERN STACK.</p>
+                    <p>E-Shop is an Ecommerce web application created using MERN STACK.</p>
                     <div className='project_item-cta'>
-                        <a href="https://github.com/AgyapalSingh/Desi_Shop" className='btn' target='_blank'>Github</a>
+                        <a href="https://github.com/AgyapalSingh/E-Shop-Hosting" className='btn' target='_blank'>Github</a>
                         <a href="https://e-shop-mern.netlify.app" className='btn btn-primary' target='_blank'>Live Demo</a>
                     </div>
                 </article>
                 <article className='project_item'>
                     <div className="project_item-image">
-                        <img src={IMG2} alt="desishop" />
+                        <img src={IMG3} alt="moviemaster" />
                     </div>
                     <h3>MovieMaster - Movie website</h3>
-                    <h5 className='text-light'>[ Complete Ecommerce Website]</h5>
-                    <p>MovieMaster is a Movie Searching webapplication created using VITE+REACT.</p>
+                    <h5 className='text-light'>[ Movie Searching Web Application]</h5>
+                    <p>MovieMaster is a Movie Searching web application created using VITE + REACT.</p>
                     <div className='project_item-cta'>
                         <a href="https://github.com/AgyapalSingh/MovieMaster" className='btn' target='_blank'>Github</a>
                         <a href="https://movie-master-pro.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
@@ -39,29 +39,17 @@ const Project = () => {
 
                 <article className='project_item'>
                     <div className="project_item-image">
-                        <img src={IMG1} alt="songify" />
+                        <img src={IMG4} alt="ipod" />
                     </div>
-                    <h3>Songify - Music App</h3>
-                    <h5 className='text-light'>[ Template for Music Web Application ]</h5>
-                    <p>Songify is music web application created using HTML, CSS and Bootstrap</p>
+                    <h3>IPOD - Music App</h3>
+                    <h5 className='text-light'>[ IPOD Music Application ]</h5>
+                    <p>Ipod is music web application created React.JS</p>
 
                     <div className='project_item-cta'>
-                        <a href="https://github.com/AgyapalSingh/songify" className='btn' target='_blank'>Github</a>
-                        <a href="https://songifymusicapp.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+                        <a href="https://github.com/AgyapalSingh/Ipod" className='btn' target='_blank'>Github</a>
+                        <a href="https://ipod-using-react.netlify.app" className='btn btn-primary' target='_blank'>Live Demo</a>
                     </div>
                 </article>
-                {/* <article className='project_item'>
-                    <div className="project_item-image">
-                        <img src={IMG3} alt="kart" />
-                    </div>
-                    <h3>KART</h3>
-                    <h5 className='text-light'>[ Template for Ecommerce Website ]</h5>
-                    <p>KART is Ecommerce website template created using HTML, CSS and JavaScript</p>
-                    <div className='project_item-cta'>
-                        <a href="https://github.com/AgyapalSingh/Shopping_Kart" className='btn' target='_blank'>Github</a>
-                        <a href="https://kartprojectsample.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
-                    </div>
-                </article> */}
 
 
 
